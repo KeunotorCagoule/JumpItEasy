7ab13c90211797d1025e11d86e71d7d4a53054ef
0xPhantasia <evan.mestre@sanofi.com>
2025-04-17 11:10:14 +0200

    Eslint config set to nodejs env

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -2,5 +2,20 @@ import eslintPluginJs from '@eslint/js';
 
 export default [
   eslintPluginJs.configs.recommended,
+  {
+    languageOptions: {
+      env: {
+        node: true,
+        es2022: true,
+      },
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+      }
+    }
+  }
 ];
-
